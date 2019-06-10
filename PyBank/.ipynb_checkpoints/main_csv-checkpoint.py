71481d2407991a3006b50415f06d53303b07d2f0
emrahselli <48597383+emrahselli@users.noreply.github.com>
2019-06-10 01:16:20 -0600

    pushing recent files

--- PyBank/.ipynb_checkpoints/main_csv-checkpoint.py
+++ PyBank/.ipynb_checkpoints/main_csv-checkpoint.py
@@ -8,23 +8,52 @@ with open(csvpath, newline='') as csvfile:
 
     next(csvreader, None)
     
-    row_counter = 0
-    total = 0
-    total2 = 0
-    change = 0
+    row = next(csvreader, None)
+    max_m = row[0]
+    min_m = row[0]
+    profit = float(row[1])
+    row_counter = 1
+    total = float(row[1])
+    change_total = 0
+    max_p = 0
+    min_p = 0
+    prev_profit = profit
     
     for row in csvreader:
         row_counter = row_counter + 1
-        total = total + int(row[1])
+        total = total + float(row[1])
         
-    for i, row in csvreader:
-        change = int(row[i+1, 1]) - int(row[i, 1])
-        total2 = total2 + change
-    average = total2 / row_counter
+        profit = float(row[1])
+        change = profit - prev_profit
+        change_total = change_total + change
+        
+        if change > max_p:
+            max_m = row[0]
+            max_p = change
+        
+        if change < min_p:
+            min_m = row[0]
+            min_p = change
+            
+        prev_profit = profit
+        
+    average = round(change_total / (row_counter - 1))
+    
         
     print("Financial Analysis")
     print("-------------------------")
     print(f'Total months: {row_counter}')
     print(f'Total: {total}')
     print(f'Average Change: $-{average}')
+    print(f'Greatest increase in profits: {max_m} (${max_p})')
+    print(f'Greatest increase in profits: {min_m} (${min_p})')
     
+file = open("pybank_output.txt", "w")
+
+file.write("Financial Analysis \n-------------------------\n")
+file.write(f'Total months: {row_counter}\n')
+file.write(f'Total: {total}\n')
+file.write(f'Average Change: $-{average}\n')
+file.write(f'Greatest increase in profit: {max_m} ({max_p})\n')
+file.write(f'Greatest decrease in profit: {min_m} ({min_p})\n')
+file.close()
